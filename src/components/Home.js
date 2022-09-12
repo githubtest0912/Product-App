b2c9343dd83d0e58d868be09f13b5e686389e540
Selva <githubtest0912>
2022-09-12 17:29:26 +0530

    added cards for product details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,52 @@
 import React from "react";
 
-
 //import { NavLink } from 'react-router-dom';
+
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  const [product, setProduct] = useState([]);
+
+  const fetchData = () => {
+    const API_URL = "https://fakestoreapi.com/products";
+    fetch(API_URL)
+      .then((response) => {
+        if (!response.ok) throw Error("Did not received expected data");
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-     Home
+    <div className="container color">
+      <div className="row">
+        {product.map((data) => (
+          <div className="col-md-3 animated fadeIn" key={data.id}>
+            <div className="card">
+              <div className="card-body">
+                <div className="avatar">
+                  <img src={data.image} className="card-img-top" alt="" />
+                </div>
+                {/* // card title */}
+
+                <h6 className="card-title">{data.title}</h6>
+                {/* <p className="card-title">{data.description}</p> */}
+                <h6 className="card-title">{data.category}</h6>
+                {/* <h6 className="card-title">{data.rating.rate}</h6>
+                <h6 className="card-title">{data.rating.count}</h6> */}
+
+                <h4 className="card-text">${data.price}</h4>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light shadow">
+      <nav className="navbar navbar-expand-lg bg-primary navbar-dark shadow">
         <div className="container">
           <button
             className="navbar-toggler"
@@ -20,12 +21,12 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">
+                <NavLink className="nav-link text-black active" aria-current="page" to="/">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/product">
+                <NavLink className="nav-link text-white" to="/product">
                  Product
                 </NavLink>
               </li>
@@ -36,12 +37,12 @@ const Navbar = () => {
               </li> */}
               </ul>
             <NavLink className="navbar-brand fw-bolder fs-4 mx-auto" to="/">
-              LOGO
+             PRODUCT
             </NavLink>
 
             <form className="d-flex justify-content-end" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-dark" type="submit">Search</button>
       </form>
 
                       </div>
